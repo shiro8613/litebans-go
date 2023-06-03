9e6cd8d235d03d718fb71f4feff9501f23f3ba0a
toms0910 <psp1070@gmail.com>
2023-06-03 18:01:45 +0900

    fix entities

--- src/database/entites/bans.go
+++ src/database/entites/bans.go
@@ -2,21 +2,44 @@ package entites
 
 type Bans struct {
 	Id					int		`db:"id"`
-	Uuid				string	`db:"uuid"`
-	Ip					string	`db:"ip"`
-	Reason 				string	`db:"reason"`
-	Banned_By_Uuid 		string	`db:"banned_by_uuid"`
-	Banned_By_Name 		string	`db:"banned_by_name"`
-	Removed_By_Name 	string	`db:"removed_by_name"`
-	Removed_By_Reason 	string	`db:"removed_by_reason"`
-	Removed_By_Date 	string	`db:"removed_by_date"`
+	Uuid				[]uint8	`db:"uuid"`
+	Ip					[]uint8	`db:"ip"`
+	Reason 				[]uint8	`db:"reason"`
+	Banned_By_Uuid 		[]uint8	`db:"banned_by_uuid"`
+	Banned_By_Name 		[]uint8	`db:"banned_by_name"`
+	Removed_By_Uuid		[]uint8	`db:"removed_by_uuid"`
+	Removed_By_Name 	[]uint8	`db:"removed_by_name"`
+	Removed_By_Reason 	[]uint8	`db:"removed_by_reason"`
+	Removed_By_Date 	[]uint8	`db:"removed_by_date"`
 	Time 				int		`db:"time"`
 	Until 				int		`db:"until"`
 	Template 			int		`db:"template"`
-	Server_Scope 		string	`db:"server_scope"`
-	Server_Origin 		string	`db:"server_origin"`
-	Silent 				bool	`db:"silent"`
-	IpBan 				bool	`db:"ipban"`
-	IpBan_Wildcard 		bool	`db:"ipban_wildcard"`
-	Active 				bool	`db:"active"`
+	Server_Scope 		[]uint8	`db:"server_scope"`
+	Server_Origin 		[]uint8	`db:"server_origin"`
+	Silent 				[]byte	`db:"silent"`
+	IpBan 				[]byte	`db:"ipban"`
+	IpBan_Wildcard 		[]byte	`db:"ipban_wildcard"`
+	Active 				[]byte	`db:"active"`
+}
+
+type BansReturn struct {
+	Id					int		
+	Uuid				string	
+	Ip					string	
+	Reason 				string	
+	Banned_By_Uuid 		string	
+	Banned_By_Name 		string	
+	Removed_By_Uuid		string	
+	Removed_By_Name 	string	
+	Removed_By_Reason 	string	
+	Removed_By_Date 	string	
+	Time 				int		
+	Until 				int		
+	Template 			int		
+	Server_Scope 		string	
+	Server_Origin 		string	
+	Silent 				string	
+	IpBan 				string	
+	IpBan_Wildcard 		string	
+	Active 				string	
 }
--- src/database/entites/history.go
+++ src/database/entites/history.go
@@ -2,6 +2,14 @@ package entites
 
 type History struct {
 	Id	 	int		`db:"id"`
+	Date	[]uint8	`db:"date"`
+	Name	[]uint8	`db:"name"`
+	Uuid	[]uint8	`db:"uuid"`
+	Ip		[]uint8	`db:"ip"`
+}
+
+type HistoryRetrun struct {
+	Id	 	int		`db:"id"`
 	Date	string	`db:"date"`
 	Name	string	`db:"name"`
 	Uuid	string	`db:"uuid"`
